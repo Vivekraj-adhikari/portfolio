0797726f20ae873643e9d99a2fe79a2036fab707
Vikky-V <vivekraj.offcial@gmail.com>
2026-03-15 00:07:48 +0530

    Trains and saves diabetes model

--- backend/training_pipeline/diabetes_training.py
+++ backend/training_pipeline/diabetes_training.py
@@ -1,3 +1,4 @@
+import joblib
 from data_pipeline.diabetes import DiabetesDataPipeline
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import StandardScaler
@@ -10,3 +11,12 @@ class DiabetesTrainingPipeline:
             ('scaler', StandardScaler()),
             ('classifier', LogisticRegression())
         ])
+
+    def train_model(self):
+        df = self.data_pipeline.data_ingestion()
+        X, y = self.data_pipeline.data_preprocessing(df)
+        X_train, X_test, y_train, y_test = self.data_pipeline.split_data(X, y)
+        self.model_pipeline.fit(X_train, y_train)
+        joblib.dump(self.model_pipeline, '../artifacts/diabetes_model.pkl')
+        
+
