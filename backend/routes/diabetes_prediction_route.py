21bde25da7725587f335d5723e71f768ed7e97fa
Vikky-V <vivekraj.offcial@gmail.com>
2026-03-18 21:58:08 +0530

    Tests API endpoint

--- backend/routes/diabetes_prediction_route.py
+++ backend/routes/diabetes_prediction_route.py
@@ -1,10 +1,12 @@
 from fastapi import APIRouter
+import json
 from prediction_pipeline.diabetes_prediction import DiabetesPredictionPipeline
 
 router = APIRouter(prefix="/predict_diabetes", tags=['Prediction'])
 
 @router.post('/')
-def predict_diabetes(input_features: list):
+def predict_diabetes(input_features):
+    input_features = json.loads(input_features)
     prediction_pipeline = DiabetesPredictionPipeline()
-    prediction = prediction_pipeline.predict_diabetes(input_features)
+    prediction = prediction_pipeline.predict_diabetes(input_features["input_features"])
     return prediction
